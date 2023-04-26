bf611b9d683293a0f4f020c2b4d175268b73ff88
Christopher Thompson <chris_thompson@bcit.ca>
2023-04-26 13:26:10 -0700

    Simplify the hello world module

--- multivariable/01_hello_world.py
+++ multivariable/01_hello_world.py
@@ -3,14 +3,8 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
 def main():
-    print_hi('PyCharm')
+    print('Hello world!')
 
 
 # Press the green button in the gutter to run the script.
